4b15cdb99229ecc89912c398cb2f815329da832c
= <=>
2020-05-22 21:42:33 +0700

    welcome interface in App

---
+++ SneakerShop/app/src/main/java/com/example/sneakershop/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.sneakershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
---
+++ SneakerShop/app/src/main/java/com/example/sneakershop/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sneakershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logo= (ImageView)findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
